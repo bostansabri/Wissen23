debe65a405aff423f484d5fa001b5ea9f76da5c0
Ahmet Sabri Bostan <107485174+bostansabri@users.noreply.github.com>
2023-05-02 01:30:32 +0300

    Add files via upload

---
+++ Wissen C# Odev 7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Wissen_C__Odev_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double hiz = 60;
+            Console.Write("Mesafeyi Giriniz (km): ");
+            double mesafe = Convert.ToDouble(Console.ReadLine());
+            double sure = mesafe / hiz;
+            Console.WriteLine("Gidilebilecek Sure: " + sure + " saat");
+        }
+    }
+}
