9d6662e6e326f19a284e2dad4f3c9fa4bdd8adec
Ahmet Sabri Bostan <107485174+bostansabri@users.noreply.github.com>
2023-05-02 01:44:24 +0300

    Add files via upload

---
+++ Wissen C# Odev 6/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Wissen_C__Odev_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soru 6 : Klavyeden girilen 3 basamakli sayinin basamaklarini ekrana basaniniz
+
+            Console.Write("Uc Basamakli Bir Sayi Giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int birler = sayi / 100;
+            int onlar = (sayi / 10) % 10;
+            int yuzler = sayi % 10;
+            Console.WriteLine("Birler Basamagi: " + birler + "\n" + "Onlar Basamagi: " + onlar + "\n" + "Yuzler Basamagi: " + yuzler);
+        }
+    }
+}
