8aa6644e72cbbe7f185da7db7c0ab7f5d6321ae8
Ahmet Sabri Bostan <107485174+bostansabri@users.noreply.github.com>
2023-05-02 01:32:46 +0300

    Add files via upload

---
+++ Wissen C# Odev 7/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Wissen_C__Odev_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soru 7 : saatte 60 km hizla giden bir aracin ekrandan girilen mesafeyi kac saatte alacagini hesaplayin
+
+            double hiz = 60;
+            Console.Write("Mesafeyi Giriniz (km): ");
+            double mesafe = Convert.ToDouble(Console.ReadLine());
+            double sure = mesafe / hiz;
+            Console.WriteLine("Gidilebilecek Sure: " + sure + " saat");
+        }
+    }
+}
