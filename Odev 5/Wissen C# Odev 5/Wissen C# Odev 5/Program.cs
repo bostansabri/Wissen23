1cd0abf528a9220a94c1191cd106c60ccf59024d
Ahmet Sabri Bostan <107485174+bostansabri@users.noreply.github.com>
2023-05-02 01:57:48 +0300

    Add files via upload

---
+++ Odev 5/Wissen C# Odev 5/Wissen C# Odev 5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Wissen_C__Odev_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Soru 5 : Ekrandan girilen 3 sayinin en buyugunu ve en kucugunu yazdirin.
+
+            Console.WriteLine("3 Tane Sayi Giriniz: ");
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+            int sayi3 = Convert.ToInt32(Console.ReadLine());
+
+            if (sayi1 > sayi2 && sayi1 > sayi3)
+            {
+                Console.WriteLine("En Buyuk Sayi: " + sayi1);
+            }
+            else if (sayi2 > sayi1 && sayi2 > sayi3)
+            {
+                Console.WriteLine("En Buyuk Sayi: " + sayi2);
+            }
+            else
+            {
+                Console.WriteLine("En Buyuk Sayi: " + sayi3);
+            }
+        }
+    }
+}
